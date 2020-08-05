59eb1ed87245b682deb98cb6ebeecc2024a21789
kevinh94 <herrerokevin94@gmail.com>
2020-08-05 18:38:26 +0200

    added sayGoodbye function

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function sayHello(firstName, lastName) {
   console.log(`Hello ${firstName} ${lastName}`);
 }
 
+function sayGoodbye(firstName, lastName) {
+  console.log(`Goodbye ${firstName} ${lastName}`);
+}
+
 module.exports = {
   sayHello,
 };
